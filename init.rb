e3db16f7d38de48473d32566a3dc524c2589c4ea
sk-ys <yskrmt@gmail.com>
2024-08-05 22:22:19 +0900

    v0.1.1

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_issue_hierarchy_filter do
   name 'Redmine Issue Hierarchy Filter'
   author 'sk-ys'
   description 'Adds issue hierarchy filter'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/sk-ys/redmine_issue_hierarchy_filter'
   author_url 'https://github.com/sk-ys'
 end
